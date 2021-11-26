a11e3b9ce398a5efe97fe9af194212381a4bbc59
Salem-Abderaouf <salamasalem84@gmail.com>
2021-11-26 23:49:16 +0100

    update splash screen

--- Screen/Splash.js
+++ Screen/Splash.js
@@ -1,12 +1,63 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { Colors } from '../Colors'
+import { Text, View, TouchableHighlight, StyleSheet, Dimensions } from 'react-native';
+import { NativeAdsManager } from 'react-native-fbads';
+
+import { Colors } from '../Colors';
+import NativeAd from '../Atoms/NativeAd';
+import Content from '../Content';
 
 const Splash = () => {
+
+  const adsManager = new NativeAdsManager("276030287583357_276037784249274", 10)
+
   return (
-    <View>
-      <Text>Splash</Text>
+    <View style={styles.wrapper}>
+      <View>
+        <Text style={styles.DescriptionText}>
+          {Content.GetStarted}
+        </Text>
+      </View>
+      <View>
+        <NativeAd
+          adsManager={adsManager}
+          adChoicePosition="bottomRight"
+          expandable={false} />
+      </View>
+
+      <TouchableHighlight
+        onPress={() => console.log('Hello')}
+        underlayColor={Colors.Primary}
+        style={styles.GetStartedButton}>
+        <Text style={styles.GetStartedText}>Get Started</Text>
+      </TouchableHighlight>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: Colors.BG,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 25,
+  },
+  DescriptionText: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: Colors.FG,
+  },
+  GetStartedButton: {
+    width: Dimensions.get('window').width - 30,
+    height: 63,
+    backgroundColor: Colors.Primary,
+    borderRadius: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  GetStartedText: {
+    color: Colors.BG,
+    fontSize: 16,
+    fontWeight: 'bold',
+  }
+})
 export default Splash;
