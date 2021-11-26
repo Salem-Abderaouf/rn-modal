47f4997d64d65cadf7c6c7a695784668ed417666
Salem-Abderaouf <salamasalem84@gmail.com>
2021-11-26 17:21:53 +0100

    Create and update ListGuid screen

---
+++ Screen/ListGuid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Colors } from '../Colors'
+
+const ListGuid = () => {
+  return (
+    <View>
+      <Text>ListGuid</Text>
+    </View>
+  )
+}
+export default ListGuid;
