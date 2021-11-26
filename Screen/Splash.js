5336275d907091cc623aeb4d3a51a394c25a4173
Salem-Abderaouf <salamasalem84@gmail.com>
2021-11-26 17:22:04 +0100

    Create and update Splash screen

---
+++ Screen/Splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Colors } from '../Colors'
+
+const Splash = () => {
+  return (
+    <View>
+      <Text>Splash</Text>
+    </View>
+  )
+}
+export default Splash;
