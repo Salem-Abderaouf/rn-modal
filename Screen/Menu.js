2e97a15c0df4255eee1c3f5f1e7a455747d40b91
Salem-Abderaouf <salamasalem84@gmail.com>
2021-11-26 17:22:16 +0100

    Create and update Menu screen

---
+++ Screen/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Colors } from '../Colors'
+
+const Menu = () => {
+  return (
+    <View>
+      <Text>Menu</Text>
+    </View>
+  )
+}
+export default Menu;
