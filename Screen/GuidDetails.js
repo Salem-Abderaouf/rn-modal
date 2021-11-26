60abf808e57c2563e50cb0cc90719248b71a5bfe
Salem-Abderaouf <salamasalem84@gmail.com>
2021-11-26 17:21:36 +0100

    Create and update GuidDetails screen

---
+++ Screen/GuidDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Colors } from '../Colors'
+
+const GuidDetails = () => {
+  return (
+    <View>
+      <Text>GuidDetails</Text>
+    </View>
+  )
+}
+export default GuidDetails;
