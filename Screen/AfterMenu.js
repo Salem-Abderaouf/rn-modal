6e9d30d2c7b10173b11a5b7891c715ed44b206c1
Salem-Abderaouf <salamasalem84@gmail.com>
2021-11-26 17:21:24 +0100

    Create and update AfterMenu screen

---
+++ Screen/AfterMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Colors } from '../Colors'
+
+const AfterDetails = () => {
+  return (
+    <View>
+      <Text>AfterDetails</Text>
+    </View>
+  )
+}
+export default AfterDetails;
