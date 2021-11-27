a3fad95517515619e1ec1ec5e9b9637ac8988163
Salem-Abderaouf <salamasalem84@gmail.com>
2021-11-27 01:16:47 +0100

    update content

--- Content.js
+++ Content.js
@@ -1,6 +1,67 @@
 const Content = {
   GetStarted: ` Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
-  MenuText: `Hello`
+  MenuText: `Menue Text`,
+  AfterMenu: `AfterMenu`,
+  OneButton: 'One',
+  TwoButton: 'Two',
+  ThreeButton: 'Three',
+  FourButton: 'Four',
+  FirstParaghraph: {
+    one: `Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+    two: `Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+    three: `Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`
+  },
+  FirstHeading: {
+    one: 'Heading',
+    two: 'Header',
+    three: 'Fourth'
+  },
+  // Two
+  SecondParaghraph: {
+    one: `Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+    two: `Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+    three: `Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`
+  },
+  SecondHeading: {
+    one: 'Heading',
+    two: 'Header',
+    three: 'Fourth'
+  },
+  // Third
+  ThirdParaghraph: {
+    one: `Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+    two: `Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+    three: `Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`
+  },
+  ThirdHeading: {
+    one: 'Heading',
+    two: 'Header',
+    three: 'Fourth'
+  },
+  // Fourth
+  FourthParaghraph: {
+    one: `Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+    two: `Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+    three: `Lorem Ipsum is simply dummy text of  the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`
+  },
+  FourthHeading: {
+    one: 'Heading',
+    two: 'Header',
+    three: 'Fourth'
+  }
+
 }
 export default Content
