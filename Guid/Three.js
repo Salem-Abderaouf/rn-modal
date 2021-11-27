ea51c3e7d87a3fe68b097848cca632b86f6f4b0e
Salem-Abderaouf <salamasalem84@gmail.com>
2021-11-27 01:17:53 +0100

    Create and update Three.js screen

---
+++ Guid/Three.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Text, View, StyleSheet, TouchableHighlight, Dimensions, ScrollView } from 'react-native';
+import { BannerView } from 'react-native-fbads';
+
+import { Colors } from '../Colors'
+import Content from '../Content';
+
+const Three = () => {
+  return (
+    <View style={styles.wrapper}>
+      <ScrollView>
+        <View style={styles.bloc}>
+          <Text style={styles.blocHeading}>{Content.ThirdHeading.one}</Text>
+          <Text style={styles.blocParagraph}>{Content.ThirdParaghraph.one}</Text>
+        </View>
+        <View style={styles.bannerContainer}>
+          <BannerView
+            placementId='276030287583357_276035734249479'
+            type="large"
+            onPress={() => console.log('click')}
+            onLoad={() => console.log('loaded')}
+            onError={(err) => console.log('error', err)}
+          />
+        </View>
+        <View style={styles.bloc}>
+          <Text style={styles.blocHeading}>{Content.ThirdHeading.two}</Text>
+          <Text style={styles.blocParagraph}>{Content.ThirdParaghraph.two}</Text>
+        </View>
+        <View style={styles.bannerContainer}>
+          <BannerView
+            placementId='276030287583357_276035734249479'
+            type="large"
+            onPress={() => console.log('click')}
+            onLoad={() => console.log('loaded')}
+            onError={(err) => console.log('error', err)}
+          />
+        </View>
+        <View style={styles.bloc}>
+          <Text style={styles.blocHeading}>{Content.ThirdHeading.three}</Text>
+          <Text style={styles.blocParagraph}>{Content.ThirdParaghraph.three}</Text>
+        </View>
+        <View style={styles.bannerContainer}>
+          <BannerView
+            placementId='276030287583357_276035734249479'
+            type="large"
+            onPress={() => console.log('click')}
+            onLoad={() => console.log('loaded')}
+            onError={(err) => console.log('error', err)}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  bloc: {
+    padding: 25,
+  },
+  blocHeading: {
+    color: Colors.Primary,
+    fontSize: 20,
+  },
+  blocParagraph: {
+    color: Colors.FG,
+    textAlign: "justify",
+    lineHeight: 25
+  },
+  bannerContainer: {
+    padding: 25,
+    alignSelf: 'center',
+    position: 'relative',
+    bottom: 0,
+    left: -200
+  }
+})
+export default Three;
