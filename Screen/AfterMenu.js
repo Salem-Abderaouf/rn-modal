83ac298780319b812f2426ddeb101decae5869b4
Salem-Abderaouf <salamasalem84@gmail.com>
2021-11-27 01:18:28 +0100

    update AfterMenu file

--- Screen/AfterMenu.js
+++ Screen/AfterMenu.js
@@ -1,12 +1,62 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { Colors } from '../Colors'
+import { Text, View, TouchableHighlight, StyleSheet, Dimensions } from 'react-native';
+import { NativeAdsManager } from 'react-native-fbads';
 
-const AfterDetails = () => {
+import { Colors } from '../Colors';
+import NativeAd from '../Atoms/NativeAd';
+import Content from '../Content';
+
+const AfterMenu = ({ navigation }) => {
+  const adsManager = new NativeAdsManager("276030287583357_276037784249274", 10)
+
   return (
-    <View>
-      <Text>AfterDetails</Text>
+    <View style={styles.wrapper}>
+      <View>
+        <Text style={styles.DescriptionText}>
+          {Content.AfterMenu}
+        </Text>
+      </View>
+      <View>
+        <NativeAd
+          adsManager={adsManager}
+          adChoicePosition="bottomRight"
+          expandable={false} />
+      </View>
+
+      <TouchableHighlight
+        onPress={() => navigation.navigate('listguid')}
+        underlayColor={Colors.Primary}
+        style={styles.GetStartedButton}>
+        <Text style={styles.GetStartedText}>Done</Text>
+      </TouchableHighlight>
     </View>
   )
 }
-export default AfterDetails;
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: Colors.BG,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 25,
+  },
+  DescriptionText: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: Colors.FG,
+  },
+  GetStartedButton: {
+    width: Dimensions.get('window').width - 30,
+    height: 63,
+    backgroundColor: Colors.Primary,
+    borderRadius: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  GetStartedText: {
+    color: Colors.BG,
+    fontSize: 16,
+    fontWeight: 'bold',
+  }
+})
+export default AfterMenu;
